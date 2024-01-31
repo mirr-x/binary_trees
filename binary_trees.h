bc7aceb63d378f07e9b8f34cad106c9a03dadd38
amir <amir.exe@icloud.com>
2024-01-31 16:13:07 +0100

    tree

--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,7 @@ struct binary_tree_s
 };
 typedef struct binary_tree_s binary_tree_t;
 
+typedef struct binary_tree_s binary_tree_t;
 
 /* prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
